3050133cd75ac9af5c9d2f4431d0dc015610d023
damiao <183851258@qq.com>
2020-12-04 13:52:06 +0800

    x

---
+++ src/Index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: uuui
+ * Date: 2020/12/4
+ * Time: 13:51
+ */
+
+namespace iiiuJwtToken;
+
+
+class Index
+{
+    
+    public function __construct()
+    {
+        echo 1;
+    }
+}
